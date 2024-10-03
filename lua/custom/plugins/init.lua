10192e6be0e63630ea2f348423395a4b7fab4ec2
Zachareee <zacharytanhx@yahoo.com>
2024-10-03 21:05:56 +0800

    Add LazyGit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,15 +9,28 @@ vim.opt.shell = 'powershell'
 vim.opt.shellxquote = ''
 vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
 require('nvim-treesitter.install').compilers = { 'clang', 'gcc' }
+-- require('which-key').add(mappings, opts?)
+
 return {
+  'andweeb/presence.nvim',
   {
-    'amrbashir/nvim-docs-view',
+    'kdheepak/lazygit.nvim',
     lazy = true,
-    cmd = 'DocsViewToggle',
-    opts = {
-      position = 'right',
-      width = 60,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
-  'andweeb/presence.nvim',
 }
