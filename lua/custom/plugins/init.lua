aac3676088328449b81431a125fa095370eac991
Zachareee <zacharytanhx@yahoo.com>
2025-06-26 23:46:59 +0900

    Use pwsh instead of powershell, change tabstop values, remove mason-nvim-dap

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,11 +5,15 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
 vim.keymap.set('n', '<leader>dt', '<cmd>diffthis<CR>', { desc = '[D]iff[T]his' })
 vim.keymap.set('n', '<leader>do', '<cmd>diffoff<CR>', { desc = '[D]iff[O]ff' })
 
 if package.config:sub(1, 1) == '\\' then
-  vim.opt.shell = 'powershell'
+  vim.opt.shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell'
   vim.opt.shellxquote = ''
   vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
 end
@@ -18,7 +22,35 @@ require('nvim-treesitter.install').compilers = { 'clang', 'gcc' }
 -- require('which-key').add(mappings, opts?)
 
 return {
-  'andweeb/presence.nvim',
+  -- {
+  --   'IogaMaster/neocord',
+  --   opts = {
+  --     global_timer = true,
+  --   },
+  -- },
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+  },
+  -- {
+  --   'kevinhwang91/nvim-ufo',
+  --   dependencies = {
+  --     'kevinhwang91/promise-async',
+  --   },
+  --   config = function()
+  --     require('ufo').setup {
+  --       provider_selector = function()
+  --         return { 'treesitter', 'indent' }
+  --       end,
+  --     }
+  --   end,
+  -- },
+  {
+    'laytan/cloak.nvim',
+    config = function()
+      require('cloak').setup { enabled = true }
+    end,
+  },
   {
     'kdheepak/lazygit.nvim',
     lazy = true,
@@ -38,5 +70,13 @@ return {
     keys = {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
+  },
+  {
+    'rmagatti/auto-session',
+    config = function()
+      require('auto-session').setup {
+        suppressed_dirs = { '~' },
+      }
+    end,
   },
 }
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -19,7 +19,7 @@ return {
 
     -- Installs the debug adapters for you
     'mason-org/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
+    -- 'jay-babu/mason-nvim-dap.nvim',
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
@@ -81,22 +81,33 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
 
-    require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
-      },
-    }
+    -- require('mason-nvim-dap').setup {
+    --   -- Makes a best effort to setup the various debuggers with
+    --   -- reasonable debug configurations
+    --   automatic_installation = true,
+    --
+    --   -- You can provide additional configuration to the handlers,
+    --   -- see mason-nvim-dap README for more information
+    --   handlers = {
+    --     function(config)
+    --       require('mason-nvim-dap').default_setup(config)
+    --     end,
+    --     python = function(config)
+    --       config.adapters = {
+    --
+    --       }
+    --       require('mason-nvim-dap').default_setup(config)
+    --     end,
+    --   },
+    --
+    --   -- You'll need to check that you have the required things installed
+    --   -- online, please don't ask me how to install them :)
+    --   ensure_installed = {
+    --     -- Update this to ensure that you have the debuggers for the langs you want
+    --     'delve',
+    --     'python',
+    --   },
+    -- }
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
